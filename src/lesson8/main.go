618f76cef19d3c3663bad4db6bf20b0dfbd5dac1
Yuzuki Nishi <youyuexi196@gmail.com>
2025-06-07 19:22:31 +0900

    feat: チャネルとゴルーチン

---
+++ src/lesson8/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func receiver(c chan int) {
+	for {
+		i := <-c
+		fmt.Println(i)
+	}
+}
+
+func main() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	go receiver(ch1)
+	go receiver(ch2)
+
+	i := 0
+	for i < 100 {
+		ch1 <- i
+		ch2 <- i
+		time.Sleep(50 * time.Microsecond)
+		i++
+	}
+
+}
